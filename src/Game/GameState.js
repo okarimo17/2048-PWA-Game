83c5622d4e8a81e8965263e90db2d09efe4d8a2a
oucifkarim01 <oucifkarim01@gmail.com>
2020-08-18 13:53:11 +0100

    Nearly Completed Algorithm

--- src/Game/GameState.js
+++ src/Game/GameState.js
@@ -33,7 +33,7 @@ function inputHandeler(ev,updateBoard){
 
 let EndIndex = 0;
 
-function getMove(i,j,val,haveToShift=false){
+function getMove(i,j,val,haveToShift=false,uniq=[]){
 
   if(i<EndIndex){
     // index is out of bound state of the array
@@ -47,23 +47,48 @@ function getMove(i,j,val,haveToShift=false){
 
 
   if(cur_val === 0){
-    console.log('eqauls to 0 first test');
-    let new_val = getMove(i-1,j,cur_val,true);
+    if(val!==0)
+      uniq.push(val)
+
+    console.log(`unique is :: ${uniq}`);
+
+    let new_val = getMove(i-1,j,cur_val,true,uniq);
+
+    console.log(`${i}:: 0 test , nvl:${new_val} shif ${haveToShift}`);
+
+    console.log(`${i}:: 00000 ${haveToShift},${uniq}`);
+
+
 
     if(new_val === val){
+      let new_val_new = getMove(i-1,j,0,true,[]);
+      board[i][j] = new_val_new;
+      return new_val;
+    } 
+
+    console.log(`${i}:: 11111 ${haveToShift}`);
+
+
+    if(haveToShift){
       return new_val;
     }
 
-    if(val === 0 || haveToShift)
-      return new_val;
+
+    console.log(`${i}:: 2222 ${haveToShift}`);
+    
+
+    if(new_val<0)
+      new_val = -new_val;
 
     board[i][j] = new_val;
+
     return 0;
   }
   
 
   
   let new_val;
+
 
   if (cur_val === val){
 
@@ -74,22 +99,38 @@ function getMove(i,j,val,haveToShift=false){
     board[i][j] = new_val;
     console.log(`${i}::newval::${new_val} curval:${cur_val}==old_val:${val}`);
 
-
     return to_ret;
 
   }
+
+  if(val===0){
+    let last = uniq.shift();
+    if(cur_val === last){
+
+      let to_ret=cur_val;
+
+      new_val = getMove(i-1,j,0,true,uniq);
+  
+      board[i][j] = new_val;
+
+      return to_ret;
+    }else {
+      uniq.unshift(last);
+    }
+  }
+
 
 
 
   new_val = getMove(i-1,j,cur_val,haveToShift);
 
-  let to_ret=cur_val;
+  let to_ret  = cur_val;
 
-  let equals = false;
+  let equals  = false;
 
   if(cur_val === new_val){
     console.log(`${i}:: curval:${cur_val}==newval:${new_val}`);
-    to_ret = new_val = cur_val = cur_val + 1;
+    new_val = cur_val = cur_val + 1;
     to_ret = 0;
     equals = true;
   }
@@ -102,25 +143,23 @@ function getMove(i,j,val,haveToShift=false){
 
 
   
-  if(val===0 || haveToShift){
+  if(haveToShift){
 
     console.log(`${i}:: shif:${haveToShift},eq:${equals}`);
 
     if(equals){
-      to_ret = new_val;
+      to_ret = -new_val;
       new_val = 0;
     }
 
   }else {
     new_val = cur_val;
-
   }
 
   board[i][j] = new_val;
 
   return to_ret;
 }
-
 
 
 
